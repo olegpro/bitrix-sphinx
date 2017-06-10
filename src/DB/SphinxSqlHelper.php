178c4db124f7e1cde0ef14512f08bb41841a6cce
Oleg Maksimenko <oleg.39style@gmail.com>
2017-06-10 19:01:21 +0200

    Добавил метод SphinxSqlHelper::escape с помощью которого можно экранировать фразу, передаваемую в MATCH('')

--- src/DB/SphinxSqlHelper.php
+++ src/DB/SphinxSqlHelper.php
@@ -12,5 +12,64 @@ use Bitrix\Main\DB\SqlHelper;
 
 class SphinxSqlHelper extends MysqliSqlHelper
 {
-    
+
+    /**
+     * Escapes special characters in a string for use in match.
+     *
+     * @param string $value Value to be escaped.
+     *
+     * @return string
+     */
+    public function escape($value)
+    {
+        static $search = [
+            "\\",
+            "'",
+            "/",
+            ")",
+            "(",
+            "$",
+            "~",
+            "!",
+            "@",
+            "^",
+            "-",
+            "|",
+            "<",
+            "\x0",
+            "=",
+        ];
+
+        static $replace = [
+            "\\\\",
+            "\\'",
+            "\\\\/",
+            "\\\\)",
+            "\\\\(",
+            "\\\\\$",
+            "\\\\~",
+            "\\\\!",
+            "\\\\@",
+            "\\\\^",
+            "\\\\-",
+            "\\\\|",
+            "\\\\<",
+            " ",
+            " ",
+        ];
+
+        $value = str_replace($search, $replace, $value);
+
+        $stat = count_chars($value, 1);
+
+        if (
+            isset($stat[ord('"')])
+            && $stat[ord('"')] % 2 === 1
+        ) {
+            $value = str_replace('"', '\\\"', $value);
+        }
+
+        return $value;
+    }
+
 }
